0a7d82e475cd06ed39195bcd6ef50e0e8a8a75b0
filmaj <maj.fil@gmail.com>
2022-08-21 10:57:07 -0400

    upgrading to architect 10

--- src/plugins/bucket-permissions.js
+++ src/plugins/bucket-permissions.js
@@ -1,60 +1,64 @@
+// godbucket with all my images
+const bucket = 'photos-filmaj-ca';
+const bucketArn = `arn:aws:s3:::${bucket}`;
+// the @events S3upload SNS topic defined in app.arc
+const s3upload = 'S3uploadEventTopic';
+
 module.exports = {
-  package: function storage ({ cloudformation: cfn }) {
-    // godbucket with all my images
-    let bucket = 'photos-filmaj-ca';
-    let bucketArn = `arn:aws:s3:::${bucket}`;
-    // the @events S3upload SNS topic defined in app.arc
-    let s3upload = 'S3uploadEventTopic';
-    // First thing we do is add permissions to our image godbucket to our role
-    cfn.Resources.PrivateStorageMacroPolicy = {
-      Type: 'AWS::IAM::Policy',
-      DependsOn: 'Role',
-      Properties: {
-        PolicyName: 'PrivateStorageMacroPolicy',
-        PolicyDocument: {
-          Statement: [ {
-            Effect: 'Allow',
-            Action: [ 's3:*' ],
-            Resource: [bucketArn, `${bucketArn}/*`]
-          } ]
-        },
-        Roles: [ { 'Ref': 'Role' } ],
-      }
-    };
-    // add a SNS topic policy for the S3uploadEventTopic for events from our godbucket
-    cfn.Resources.S3uploadEventTopicPolicy = {
-      Type: 'AWS::SNS::TopicPolicy',
-      DependsOn: 'Role',
-      Properties: {
-        PolicyDocument: {
-          Version: '2012-10-17',
-          Statement: [ {
-            Effect: 'Allow',
-            Principal: {
-              Service: 's3.amazonaws.com'
-            },
-            Action: [ 'sns:Publish' ],
-            Resource: {'Ref': s3upload },
-            Condition: {
-              'ArnLike': {
-                'aws:SourceArn': `arn:aws:s3:*:*:${bucket}`
+  deploy: {
+    start: function storage ({ cloudformation: cfn }) {
+      // console.log('bucket plugin cfn', cfn);
+      // First thing we do is add permissions to our image godbucket to our role
+      cfn.Resources.PrivateStorageMacroPolicy = {
+        Type: 'AWS::IAM::Policy',
+        DependsOn: 'Role',
+        Properties: {
+          PolicyName: 'PrivateStorageMacroPolicy',
+          PolicyDocument: {
+            Statement: [ {
+              Effect: 'Allow',
+              Action: [ 's3:*' ],
+              Resource: [bucketArn, `${bucketArn}/*`]
+            } ]
+          },
+          Roles: [ { 'Ref': 'Role' } ],
+        }
+      };
+      // add a SNS topic policy for the S3uploadEventTopic for events from our godbucket
+      cfn.Resources.S3uploadEventTopicPolicy = {
+        Type: 'AWS::SNS::TopicPolicy',
+        DependsOn: 'Role',
+        Properties: {
+          PolicyDocument: {
+            Version: '2012-10-17',
+            Statement: [ {
+              Effect: 'Allow',
+              Principal: {
+                Service: 's3.amazonaws.com'
+              },
+              Action: [ 'sns:Publish' ],
+              Resource: {'Ref': s3upload },
+              Condition: {
+                'ArnLike': {
+                  'aws:SourceArn': `arn:aws:s3:*:*:${bucket}`
+                }
               }
-            }
-          } ]
-        },
-        Topics: [ { 'Ref': 'S3uploadEventTopic' } ],
-      }
-    };
+            } ]
+          },
+          Topics: [ { 'Ref': s3upload } ],
+        }
+      };
 
-    let resKeys = Object.keys(cfn.Resources);
+      let resKeys = Object.keys(cfn.Resources);
 
-    // Add PHOTO_BUCKET env var with bucket name to all lambdas
-    resKeys.forEach((k) => {
-      if (cfn.Resources[k].Type === 'AWS::Serverless::Function') {
-        cfn.Resources[k].Properties.Environment.Variables['PHOTO_BUCKET'] = bucket;
-      }
-    });
+      // Add PHOTO_BUCKET env var with bucket name to all lambdas
+      resKeys.forEach((k) => {
+        if (cfn.Resources[k].Type === 'AWS::Serverless::Function') {
+          cfn.Resources[k].Properties.Environment.Variables['PHOTO_BUCKET'] = bucket;
+        }
+      });
 
-    return cfn;
+      return cfn;
+    }
   }
 };
