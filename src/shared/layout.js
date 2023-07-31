e6bd783035433b8c4b2a1f1488410a824c90889e
filmaj <maj.fil@gmail.com>
2023-07-31 08:46:48 -0400

    replace google analytics with goatcounter.

--- src/shared/layout.js
+++ src/shared/layout.js
@@ -20,7 +20,7 @@ module.exports = function layout({ title, body, scripts, head, req }) {
   <link rel="icon" type="image/png" sizes="16x16" href="${arc.static('favicons/favicon-16x16.png')}">
   <link rel="stylesheet" href="${arc.static('index.css')}" />
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ==" crossorigin=""/>
-  ${(process.env.ARC_ENV === 'production' ? '<script async src="https://www.googletagmanager.com/gtag/js?id=G-GPE1GXNEM5"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag("js", new Date());gtag("config", "G-GPE1GXNEM5");</script>' : '')}
+  ${(process.env.ARC_ENV === 'production' ? '<script data-goatcounter="https://filmaj-photos.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>' : '')}
   <meta property="og:site_name" content="Fil Maj's Photos" />
   <meta property="og:type" content="website" />
   <meta property="og:url" content="https://${process.env.ARC_ENV == 'production' ? 'photos.filmaj.ca' : 'photos-staging.filmaj.ca'}${req.path}" />
