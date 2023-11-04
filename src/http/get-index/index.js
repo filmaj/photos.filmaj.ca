07cda7b54a15e9f472c7d4a5b64ef331b671a85d
filmaj <maj.fil@gmail.com>
2023-11-04 08:46:00 -0400

    basic cache on index page.

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -7,6 +7,7 @@ const imgBase = 'https://photos-img.filmaj.ca';
 const Delimiter = '/';
 const listOptions = { Bucket, Delimiter };
 const s3 = new aws.S3();
+let cache = '';
 
 async function getIndex (req) {
   let title = 'Fil Maj\'s Photo Albums';
@@ -14,25 +15,29 @@ async function getIndex (req) {
     `<meta property="og:title" content="${title}" />`,
     '<meta name="author" content="Filip Maj">',
   ];
-  let keys = await s3.listObjectsV2(listOptions).promise();
   let albums = 'No Albums :(';
-  if (keys.Contents.length === 0 && keys.CommonPrefixes.length) {
-    // lets list albums
-    albums = '';
-    const images = keys.CommonPrefixes.reverse();
-    for (let i = 0; i < images.length; i++) {
-      const p = images[i];
-      const idx = p.Prefix.lastIndexOf('-');
-      const label = p.Prefix.substring(0, idx) + ': ' + p.Prefix.substring(idx + 1, p.Prefix.length - 1);
-      const cover = await imageUtils.cover(Bucket, p.Prefix, imageUtils.TILE, s3);
-      albums += `<li>
-        <a href="/${p.Prefix}">
-          <img src="${imgBase}/${p.Prefix}${cover}" />
-          <p>${label}</p>
-        </a>
-      </li>`;
+  if (cache.length) {
+    albums = cache;
+  } else {
+    let keys = await s3.listObjectsV2(listOptions).promise();
+    if (keys.Contents.length === 0 && keys.CommonPrefixes.length) {
+      // lets list albums
+      albums = '';
+      const images = keys.CommonPrefixes.reverse();
+      for (let i = 0; i < images.length; i++) {
+        const p = images[i];
+        const idx = p.Prefix.lastIndexOf('-');
+        const label = p.Prefix.substring(0, idx) + ': ' + p.Prefix.substring(idx + 1, p.Prefix.length - 1);
+        const cover = await imageUtils.cover(Bucket, p.Prefix, imageUtils.TILE, s3);
+        albums += `<li>
+          <a href="/${p.Prefix}">
+            <img src="${imgBase}/${p.Prefix}${cover}" />
+            <p>${label}</p>
+          </a>
+        </li>`;
+      }
+      albums = `<ul id="gallery">${albums}</ul>`;
     }
-    albums = `<ul id="gallery">${albums}</ul>`;
   }
   return layout({ title, body: albums, req, head });
 }
